ac56e3171b8a2ce29dc75949ff644bc7091a418d
Alvaro Leiva <alvaroflmiranda@gmail.com>
2016-06-19 21:04:43 -0400

    refactored to be compatible with Freedesktop Icon Theme Specification Version 0.11

--- icons.go
+++ icons.go
@@ -1,10 +1,11 @@
 package freedesktop
 
 import (
-	"io/ioutil"
+	"fmt"
 	"os"
 	"os/exec"
 	"path"
+	"strconv"
 	"strings"
 )
 
@@ -35,11 +36,12 @@ func GetIconTheme() string {
 }
 
 func AppIcon(icon string) string {
-	return AppIconForSize(icon, "48x48")
+	return AppIconForSize(icon, 48)
 }
 
-func AppIconForSize(icon, size string) (filename string) {
+func AppIconForSize(icon string, size int) (filename string) {
 	theme := GetIconTheme()
+	fmt.Println("usign theme:", theme)
 	if theme != "" {
 		filename = FindIconHelper(icon, size, theme)
 		if filename != "" {
@@ -55,7 +57,7 @@ func AppIconForSize(icon, size string) (filename string) {
 	return LookupFallbackIcon(icon)
 }
 
-func FindIconHelper(icon, size, theme string) (filename string) {
+func FindIconHelper(icon string, size int, theme string) (filename string) {
 	filename = LookupIcon(icon, size, theme)
 	if filename != "" {
 		return
@@ -66,7 +68,7 @@ func FindIconHelper(icon, size, theme string) (filename string) {
 	return ""
 }
 
-func LookupIcon(icon, size, theme string) string {
+func LookupIcon(icon string, size int, theme string) string {
 	themeDirs := make([]string, 0)
 	for _, dir := range xdgIcons {
 		themeDir := path.Join(dir, theme)
@@ -78,23 +80,30 @@ func LookupIcon(icon, size, theme string) string {
 	}
 
 	for _, themeDir := range themeDirs {
-		sizeDir := path.Join(themeDir, size)
-		if _, err := os.Stat(sizeDir); err != nil {
-			// size not found
-			continue
-		}
-
-		categories, err := ioutil.ReadDir(sizeDir)
+		themeConf, err := ParseConfigFile(path.Join(themeDir, "index.theme"))
 		if err != nil {
-			// couldn't read directory
+			// no index.theme
 			continue
 		}
 
-		for _, category := range categories {
-			categoryDir := path.Join(sizeDir, category.Name())
+		var lookupSubDirs []string
+		for header, conf := range themeConf {
+			if header != "Icon Theme" {
+				themeSize := conf["Size"]
+				if themeSize == strconv.Itoa(size) {
+					lookupSubDirs = append(lookupSubDirs, header)
+				}
+			}
+		}
 
+		if len(lookupSubDirs) == 0 {
+			// No sizes matchs
+			continue
+		}
+
+		for _, subDir := range lookupSubDirs {
 			for _, ext := range xdgIconExtensions {
-				file := path.Join(categoryDir, icon+ext)
+				file := path.Join(themeDir, subDir, icon+ext)
 				if _, err := os.Stat(file); err == nil {
 					return file
 				}
